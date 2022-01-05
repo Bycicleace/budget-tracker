a0e347929c9d6b3aac54e1308e68e09c9ad64eb9
kvammee <elliott.kvamme@gmail.com>
2022-01-05 14:08:36 -0600

    Added caching of files

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,11 +3,16 @@ const VERSION = 'version-01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js"
 ];
 
 self.addEventListener('install', function(event) {
-    // Install
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    )
 });
 
 self.addEventListener('activate', function(event) {

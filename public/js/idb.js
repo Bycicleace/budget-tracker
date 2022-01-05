bda6710f9693e77dfbedb0531c1de941870258cf
kvammee <elliott.kvamme@gmail.com>
2022-01-05 10:48:43 -0600

    Added some indexedDB functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,3 +6,30 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.online) {
+        // uploadTransaction
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+}
+
+function saveRecord(record) {
+    const dbTran = db.transaction(['new_transaction'], 'readwrite');
+    const tranObjectStore = dbTran.objectStore('new_transaction');
+    tranObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    // Get all the transactions in the IndexedDB db
+    const dbTran = db.transaction(['new_transaction'], 'readwrite');
+    const tranObjectStore = dbTran.objectStore('new_transaction');
+    const getAll = tranObjectStore.getAll();
+
+
+}

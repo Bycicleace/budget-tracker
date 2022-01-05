f7f5a32d33f30312eb78f05bdc3bccdfe14eea85
kvammee <elliott.kvamme@gmail.com>
2022-01-05 13:58:55 -0600

    Added basic skeleton to service-worker file

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version-01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+
+];
+
+self.addEventListener('install', function(event) {
+    // Install
+});
+
+self.addEventListener('activate', function(event) {
+    // Activate
+});
+
+self.addEventListener('fetch', function(event) {
+    // Handling requests
+});
+

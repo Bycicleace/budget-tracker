a3474b8b30192e89bc0340781dc8c94e3fac67ee
kvammee <elliott.kvamme@gmail.com>
2022-01-06 10:51:30 -0600

    Updated Service Worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,12 +6,16 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/index.js",
-    "./js/idb.js"
+    "./js/idb.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(() => console.log("Cache created"))
+            .catch(function(err) {console.log("Cache Error: " + err)})
     )
 });
 
@@ -25,17 +29,29 @@ self.addEventListener('activate', function(event) {
                 cacheKeepList.push(CACHE_NAME);
                 return Promise.all(
                     keyList.map(function(key, i) {
-                        return caches.delete(keyList[i]);
+                        if (cacheKeepList.indexOf(key) === -1) {
+                            return caches.delete(keyList[i]);
+                        }  
                     })
                 );
             })
+            .catch(err => console.log("Cache Deletion error: " + err))
             
     );
 });
 
 self.addEventListener('fetch', function(event) {
+    //console.log(event.request.url);
     event.respondWith(
-        caches.match(event.request).then((request) => request || fetch(e.request))
+        caches.match(event.request).then(function (request) {
+            if (request) {
+                console.log("responded with cache: " + event.request.url);
+                return request;
+            } else {
+                console.log("responded with server: " + event.request.url);
+                return fetch(event.request);
+            }
+        })
     );
 });
 
